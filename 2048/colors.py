19738f4d951b668253a80a3150e96280e004c966
janaSunrise <warriordefenderz@gmail.com>
2021-04-02 00:17:37 +0530

    Added color theming for the game.

---
+++ 2048/colors.py
@@ -0,0 +1,57 @@
+GRID_COLOR = "#a39489"
+EMPTY_CELL_COLOR = "#C2B3A9"
+
+WINNER_BG = "#FFCC00"
+LOSER_BG = "#A39489"
+
+SCORE_LABEL_FONT = "Verdana", 20
+SCORE_FONT = "Helvetica", 32, "bold"
+
+GAME_OVER_FONT = "Helvetica", 48, "bold"
+GAME_OVER_FONT_COLOR = "#FFFFFF"
+
+# Cell number based fonts
+CELL_NUMBER_FONTS = {
+    2: ("Helvetica", 55, "bold"),
+    4: ("Helvetica", 55, "bold"),
+    8: ("Helvetica", 55, "bold"),
+    16: ("Helvetica", 50, "bold"),
+    32: ("Helvetica", 50, "bold"),
+    64: ("Helvetica", 50, "bold"),
+    128: ("Helvetica", 45, "bold"),
+    256: ("Helvetica", 45, "bold"),
+    512: ("Helvetica", 45, "bold"),
+    1024: ("Helvetica", 40, "bold"),
+    2048: ("Helvetica", 40, "bold")
+}
+
+# Cell colors theming
+
+CELL_COLORS = {
+    2: "#FceFE6",
+    4: "#F2E8CB",
+    8: "#F5B682",
+    16: "#F29446",
+    32: "#FF775C",
+    64: "#E64C2E",
+    128: "#EDE291",
+    256: "#FCE130",
+    512: "#FFDB4A",
+    1024: "#F0B922",
+    2048: "#FAD74D"
+}
+
+# Cell number-colors theming
+CELL_NUMBER_COLORS = {
+    2: "#695C57",
+    4: "#695C57",
+    8: "#FFFFFF",
+    16: "#FFFFFF",
+    32: "#FFFFFF",
+    64: "#FFFFFF",
+    128: "#FFFFFF",
+    256: "#FFFFFF",
+    512: "#FFFFFF",
+    1024: "#FFFFFF",
+    2048: "#FFFFFF"
+}
