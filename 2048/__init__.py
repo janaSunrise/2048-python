934fbcb18aac92e6ba1d9861f1b6b0b5b60c4415
janaSunrise <warriordefenderz@gmail.com>
2021-04-02 00:26:36 +0530

    Added placeholders for control, worked on generation of the graphics.

--- 2048/__init__.py
+++ 2048/__init__.py
@@ -18,8 +18,47 @@ class Game(tk.Frame):
         self.generate_gui()
         self.start_game()
 
+        self.master.bind("<Left>", self.left)
+        self.master.bind("<Right>", self.right)
+        self.master.bind("<Up>", self.up)
+        self.master.bind("<Down>", self.down)
+
+        self.mainloop()
+
     def generate_gui(self):
-        pass
+        # -- Generate the cells --
+        self.cells = []
+
+        for i in range(4):
+            row = []
+            for j in range(4):
+                cell_frame = tk.Frame(
+                    self.main_grid,
+                    width=100,
+                    height=100,
+                    bg=colors.EMPTY_CELL_COLOR
+                )
+                cell_frame.grid(row=i, column=j, padx=5, pady=5)
+
+                cell_number = tk.Label(self.main_grid, bg=colors.EMPTY_CELL_COLOR)
+                cell_number.grid(row=i, column=j)
+
+                cell_data = {"frame": cell_frame, "number": cell_number}
+
+                row.append(cell_data)
+
+        # -- Score section --
+        score_frame = tk.Frame(self)
+        score_frame.place(relx=0.5, y=40, anchor="center")
+
+        tk.Label(
+            score_frame,
+            text="Score",
+            font=colors.SCORE_LABEL_FONT
+        ).grid(row=0)
+
+        self.score_label = tk.Label(score_frame, text="0", font=colors.SCORE_FONT)
+        self.score_label.grid(row=1)
 
     def start_game(self):
         pass
